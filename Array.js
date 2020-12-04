a877bab1d87709a3cc89b2fb5d62ec88da36344e
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2020-12-04 23:18:03 +0530

    Add files via upload
    Changing Parameters to Array

---
+++ Array.js
@@ -0,0 +1,15 @@
+/**
+Pair Management
+Given two arguments, return an array which contains these two arguments.
+Examples
+makePair(1, 2) ? [1, 2]
+makePair(51, 21) ? [51, 21]
+makePair(512124, 215) ? [512124, 215]
+Notes
+N/A
+**/
+Solution
+function makePair(num1, num2) {
+	let array = [num1 , num2];
+	return array;
+}
